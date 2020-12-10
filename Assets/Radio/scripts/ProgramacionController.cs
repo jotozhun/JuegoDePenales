9bbb7367a3e4c16c18365360500606adb956aeea
OscarPallazhco <oepallaz@gmail.com>
2020-12-10 12:12:35 -0500

    cambios en vista emisoras

--- Assets/Radio/scripts/ProgramacionController.cs
+++ Assets/Radio/scripts/ProgramacionController.cs
@@ -12,22 +12,35 @@ using TMPro;
 public class ProgramacionController : MonoBehaviour
 {
     public GameObject segmentPrefab;
+    public GameObject locutorPrefab;
+    public GameObject provinciaEmisoraPrefab;   //no necesita estar instanciado en unity
+    public GameObject emisora2Prefab;           //no necesita estar instanciado en unity
     private List<GameObject> segmentObjects = new List<GameObject>();
+    public List<Emisora> emisoras;
+    public List<string> provincias;
+    public Dictionary<string, List<Emisora>> emisorasxProv = new Dictionary<string, List<Emisora>>();
 
     public int idSegmento;
+    public int idEmisora; //id en la base de datos de la emisora que se quiere obtener los segmentos actuales
     public TMP_Text titulo;
     public TMP_Text descripcion;
     public TMP_Text horarios;
     public TMP_Text noLocutores;
+    public Text nombreEmisora;
+    public Text ciudadEmisora;
+    public Text frecuenciaEmisora;
     public RawImage segmentoImage;
     public RectTransform locutoresContainer;
     public RectTransform imagenes;
-    public RectTransform content;
-    public GameObject locutorPrefab;
-    public GameObject objetoInfoPrograma;
+    public RectTransform contentSegmento;
+    public RectTransform contentEmisorasProvincia;
+    
+    public GameObject EmisorasProvinciasContainer;
+    public GameObject EmisoraContainer;
+    public GameObject InfoSegmentoContainer;
 
     void Awake() {
-        StartCoroutine(GetRequest(DisplaySegments));
+        StartCoroutine(GetEmisoras(DisplayEmisorasxProvincia));
     }
 
     // Start is called before the first frame update
@@ -42,11 +55,87 @@ public class ProgramacionController : MonoBehaviour
         currentSegmentTransmiting();
     }
 
+    IEnumerator GetEmisoras(Action<EmisorasModel> onSuccess)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get("https://oscarp.pythonanywhere.com/api/emisoras/"))
+        {
+            yield return webRequest.SendWebRequest();
+            
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log("Resultado: " + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                byte[] result = webRequest.downloadHandler.data;
+                string emisorasJSON = System.Text.Encoding.Default.GetString(result);
+                EmisorasModel info = JsonUtility.FromJson<EmisorasModel>("{\"emisoras\":" + emisorasJSON +"}");
+                for (int i = 0; i < info.emisoras.Count; i++)
+                {
+                    string key = info.emisoras[i].provincia;
+                    List<Emisora> value;
+                    if (!emisorasxProv.TryGetValue(key, out value))
+                    {
+                        value = new List<Emisora>();
+                        emisorasxProv[key] = value;
+                    }
+                    value.Add(info.emisoras[i]);
+                }
+                onSuccess(info);
+            }
+        }
+    }
+
+    public void DisplayEmisorasxProvincia(EmisorasModel info){
+        emisoras = info.emisoras;
+        float height  = 0f;
+        foreach( KeyValuePair<string, List<Emisora>> kvp in emisorasxProv )
+        {
+            Debug.Log(kvp.Key);            
+            GameObject provEmiItem = Instantiate(provinciaEmisoraPrefab) as GameObject;
+            provEmiItem.transform.SetParent(contentEmisorasProvincia, false);
+            provEmiItem.transform.Find("ProvinciaLabel").GetComponent<TextMeshProUGUI>().text = kvp.Key;
+            GameObject emisorasGameObject = provEmiItem.transform.Find("Emisoras").gameObject;
+            height += provEmiItem.GetComponent<RectTransform>().sizeDelta.y; /*sumar el tamaño del prefab ProvinciaEmisoras al content*/
+            int cantEmisoras = kvp.Value.Count;
+            float n = cantEmisoras/2;
+            if (cantEmisoras%2 == 0)
+            {
+                n-=1;
+            }
+            n = n * (emisora2Prefab.GetComponent<RectTransform>().sizeDelta.y); 
+            height += n; /*sumar el tamaño de los prefabs Emisora2 al content*/
+            height += 50f; /*sumar el spacing entre prefabs*/
+            provEmiItem.GetComponent<RectTransform>().sizeDelta = new Vector2(provEmiItem.GetComponent<RectTransform>().sizeDelta.x, provEmiItem.GetComponent<RectTransform>().sizeDelta.y + n);            
+            emisorasGameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(emisorasGameObject.GetComponent<RectTransform>().sizeDelta.x, emisorasGameObject.GetComponent<RectTransform>().sizeDelta.y + n);            
+            foreach (var item in kvp.Value)
+            {
+                Debug.Log(item.nombre);
+                GameObject emisora2Item = Instantiate(emisora2Prefab) as GameObject;
+                emisora2Item.transform.SetParent(emisorasGameObject.transform, false);
+                emisora2Item.transform.Find("emisora").GetComponent<TextMeshProUGUI>().text = item.nombre;
+                emisora2Item.transform.Find("frecuencia").GetComponent<TextMeshProUGUI>().text = item.frecuencia_dial+" "+item.tipo;
+                emisora2Item.transform.Find("btn").GetComponent<Button>().onClick.AddListener(() => {
+                    idEmisora = item.id;
+                    nombreEmisora.text = item.nombre;
+                    ciudadEmisora.text = item.ciudad;
+                    frecuenciaEmisora.text = item.frecuencia_dial+" "+item.tipo;
+                    StartCoroutine(GetRequest(DisplaySegments));
+                    EmisoraContainer.SetActive(true);
+                    EmisorasProvinciasContainer.SetActive(false);
+                });
+                emisora2Item.SetActive(true);
+            }
+            provEmiItem.SetActive(true);
+        }
+        contentEmisorasProvincia.sizeDelta = new Vector2(contentEmisorasProvincia.sizeDelta.x, height);
+    }
+
+
     IEnumerator GetRequest(Action<SegmentoModel> onSuccess)
     {
-        int idEmisora = 13; //id en la base de datos de la emisora que se quiere obtener los segmentos actuales
         // using (UnityWebRequest webRequest = UnityWebRequest.Get(String.Format("http://127.0.0.1:8000/api/emisoras/{0}/segmentos/today?format=json", idEmisora)))
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(String.Format("http://oscarp.pythonanywhere.com/api/emisoras/{0}/segmentos/today?format=json", idEmisora)))
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(String.Format("https://oscarp.pythonanywhere.com/api/emisoras/{0}/segmentos/today?format=json", idEmisora)))
         {
             // Request and wait for the desired page.
             yield return webRequest.SendWebRequest();
@@ -82,10 +171,11 @@ public class ProgramacionController : MonoBehaviour
             string horarioFormat = info.segmentos[i].horarios[0].fecha_inicio.Substring(0, 5) + " - " + info.segmentos[i].horarios[0].fecha_fin.Substring(0, 5);
             segmentItem.transform.Find("horario").GetComponent<TextMeshProUGUI>().text = horarioFormat;
             segmentItem.transform.Find("programaBtn").GetComponent<Button>().onClick.AddListener(() => {
-                objetoInfoPrograma.SetActive(true);
                 idSegmento = idActual;
                 StartCoroutine(GetSegmento(MostrarSegmento));
                 StartCoroutine(GetLocutores(MostrarLocutores));
+                InfoSegmentoContainer.SetActive(true);
+                EmisoraContainer.SetActive(false);
             });
             GameObject transmisionGameObject = segmentItem.transform.Find("entransmision").gameObject;
             Horario horarioSegmento = info.segmentos[i].horarios[0];
@@ -96,8 +186,8 @@ public class ProgramacionController : MonoBehaviour
 
         /*Dar espaciado entre segmentos*/
         RectTransform container;
-        float prefabHeight = 65f;
-        float spacing = 0f;
+        float prefabHeight = segmentPrefab.GetComponent<RectTransform>().sizeDelta.y;
+        float spacing = 1f;
         float containerHeight = (prefabHeight + spacing) * (segmentsCount);
         container = (RectTransform)segmentPrefab.transform.parent;
         container.sizeDelta = new Vector2(container.sizeDelta.x, containerHeight);
@@ -151,7 +241,7 @@ public class ProgramacionController : MonoBehaviour
      IEnumerator GetSegmento(Action<Segmento> onSuccess)
     {
         //TODO hacer un endpoint para solo consultar por un segmento por su id, con este endpoint se obtienen todos los segmentos
-        using (UnityWebRequest webRequest = UnityWebRequest.Get("http://oscarp.pythonanywhere.com/api/segmentos/"))
+        using (UnityWebRequest webRequest = UnityWebRequest.Get("https://oscarp.pythonanywhere.com/api/segmentos/"))
         {
             yield return webRequest.SendWebRequest();
             
@@ -200,7 +290,7 @@ public class ProgramacionController : MonoBehaviour
     IEnumerator GetLocutores(Action<LocutoresModel> onSuccess)
     {
         //TODO hacer un endpoint para solo consultar por un segmento por su id, con este endpoint se obtienen todos los segmentos
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(String.Format("http://oscarp.pythonanywhere.com/api/segmentos/{0}/locutores", idSegmento)))
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(String.Format("https://oscarp.pythonanywhere.com/api/segmentos/{0}/locutores", idSegmento)))
         {
             yield return webRequest.SendWebRequest();
             
@@ -236,7 +326,7 @@ public class ProgramacionController : MonoBehaviour
             n = n * (350f);
             // locutoresContainer.sizeDelta = new Vector2(locutoresContainer.sizeDelta.x, locutoresContainer.sizeDelta.y + n);
             // imagenes.sizeDelta = new Vector2(imagenes.sizeDelta.x, imagenes.sizeDelta.y + n);
-            content.sizeDelta = new Vector2(content.sizeDelta.x, content.sizeDelta.y + n);
+            contentSegmento.sizeDelta = new Vector2(contentSegmento.sizeDelta.x, contentSegmento.sizeDelta.y + n);
             for (int i = 0; i < cantLocutores; i++)
             {  
                 GameObject locutorItem = Instantiate(locutorPrefab) as GameObject;
